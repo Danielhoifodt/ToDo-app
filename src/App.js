e1a59e9541a067b3bb7ceab6f2215b1c589437db
Danielhoifodt <40982934+Danielhoifodt@users.noreply.github.com>
2019-11-06 19:54:23 +0100

    Prøver å oppdatere

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+
+state = {
+  works:[]
+}
+/*onChange=(e)=>{
+  this.setState({works: e.target.value});
+}*/
+
+add = (e) => {
+  e.preventDefault();
+  var title = this.refs.title.value;
+  if(localStorage.getItem('works') == null){
+    var works = [];
+    works.push(title);
+    localStorage.setItem('works', JSON.stringify(works));
+  }else{
+     works = JSON.parse(localStorage.getItem('works'));
+    works.push(title);
+    localStorage.setItem('works', JSON.stringify(works));
+  }
+  this.setState({works: JSON.parse(localStorage.getItem('works'))})
+  
+}
+componentDidMount(){
+  this.setState({works: JSON.parse(localStorage.getItem('works'))})
+}
+
+
+render(){
+
+  let li = this.state.works.map((work, index)=>{
+    return <li key={index}>{work}</li>
+    })
+  return(
+    <div>
+      <h1>My ToDo List</h1>
+      <form onSubmit={this.add}>
+        <input type="text" placeholder="Title..." ref="title"/>
+        <input type="submit" value="Add" onclick={this.add} />
+      </form>
+      
+      <br></br>
+      <ul>
+        {li}
+      </ul>
     </div>
   );
+}
+
+
+
+
+
+
+
+
+
+
 }
 
 export default App;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+
+class AddTodo extends Component {
+    state = {
+        title: ""
+    }
+
+    onChange = (e) => this.setState({title: e.target.value});
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title: ""});
+    }
+
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input type="text" onChange={this.onChange}></input>
+                <input type="submit" value="submit" ></input>
+            </form>
+        );
+        
+        
+}
+}
+
+
+export default AddTodo;
